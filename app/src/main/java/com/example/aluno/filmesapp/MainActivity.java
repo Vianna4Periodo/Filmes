36c5bfeaa7dcf514023ab21747bf63c78a509995
aluno <aluno@labivj.info>
2018-03-07 21:34:35 -0300

    Implementando binds da tela inicial.

--- app/src/main/java/com/example/aluno/filmesapp/MainActivity.java
+++ app/src/main/java/com/example/aluno/filmesapp/MainActivity.java
@@ -2,12 +2,25 @@ package com.example.aluno.filmesapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btLogin;
+    TextView tvEmail, tvSenha;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setupBinds();
     }
+
+    private void setupBinds() {
+        btLogin = findViewById(R.id.btLogin);
+        tvEmail = findViewById(R.id.tvEmail);
+        tvSenha = findViewById(R.id.tvPassword);
+    }
+
 }
